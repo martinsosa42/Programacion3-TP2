f65711feb3ed3ea32bee77e2498cbf0ed4b04161
Martin <sosamartin9910@gmail.com>
2026-05-10 09:24:29 -0300

    feat: implementación de procesamiento de resultados de F1
    - Clase Resultado con métodos dunder(__str__ y __repr__) .
    - Lógica con diccionarios para calcular y mostrar el Top 3 de pilotos y escuderías más ganadoras.

--- ejercicio 2/procesador_csv.py
+++ ejercicio 2/procesador_csv.py
@@ -20,3 +20,46 @@ if os.path.exists(ruta_archivo):
             resultado = Resultado(*res)
             resultados.append(resultado)
 
+# d) Mostrar los ultimos 20 resultados.
+print("--- ULTIMOS 20 RESULTADOS ---")
+for res in resultados[-20:]:
+    print (res)
+print("\n")
+
+# e) Con diccionarios obtener los 3 pilotos mas ganadores.
+pilotos_ganadores = {}
+for r in resultados:
+    if r.position == "1":
+        pilotos_ganadores[r.driver_name] = (
+            pilotos_ganadores.get(r.driver_name, 0) + 1
+        )
+
+pilotos_ganadores = sorted(
+    pilotos_ganadores.items(),
+    key=lambda x: x[1],
+    reverse=True
+)[:3]
+
+print("--- TOP 3 PILOTOS MAS GANADORES ---")
+for piloto, victorias in pilotos_ganadores:
+    print(f"{piloto}: {victorias} victorias")
+print("\n")
+
+# f) Con diccionarios obtener las 3 escuderias mas ganadores.
+escuderias_ganadoras = {}
+for r in resultados:
+    if r.position == "1":
+        escuderias_ganadoras[r.constructor] = (
+            escuderias_ganadoras.get(r.constructor, 0) + 1
+        )
+
+escuderias_ganadoras = sorted(
+    escuderias_ganadoras.items(),
+    key=lambda x: x[1],
+    reverse=True
+)[:3]
+
+print("--- TOP 3 ESCUDERIAS MAS GANADORAS ---")
+for escuderia, victorias in escuderias_ganadoras:
+    print(f"{escuderia}: {victorias} victorias")
+print("\n")
